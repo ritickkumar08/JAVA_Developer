f72e5a34c9d35cf34d220302d296890d30e22f36
ritickkumar <ritickkumar08@gmail.com>
2026-01-04 22:31:55 +0530

    4 jan,

---
+++ src/StriverA2z/january2026/Arrayss/MaxProfit.java
@@ -0,0 +1,21 @@
+package StriverA2z.january2026.Arrayss;
+
+class MaxProfit {
+    public static void main(String[] args) {
+        int[] prices =  {1,2,3,4,5};
+        MaxProfit ob = new MaxProfit();
+        int profit = ob.maxProfit(prices);
+        System.out.println(profit);
+    }
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int n = prices.length;
+        for (int i = 1; i < n; i++) {
+            if(prices[i] > prices[i-1]){
+                maxProfit += prices[i] - prices[i-1];
+            }
+        }
+
+        return maxProfit;
+    }
+}
